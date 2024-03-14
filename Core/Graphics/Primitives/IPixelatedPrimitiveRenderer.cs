156eb13479bc9aba5ff408d1a76ed740d5ff8d05
Toasty <louscliff@gmail.com>
2024-03-14 08:22:18 +0000

    - Implemented primitive system.

---
+++ Core/Graphics/Primitives/IPixelatedPrimitiveRenderer.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace Luminance.Core.Graphics
+{
+    /// <summary>
+    /// Use to sucessfully render primitives with pixelation with an NPC or Projectile.
+    /// </summary>
+    public interface IPixelatedPrimitiveRenderer
+    {
+        /// <summary>
+        /// The layer to render the primitive(s) to.
+        /// </summary>
+        PixelationPrimitiveLayer LayerToRenderTo => PixelationPrimitiveLayer.BeforeProjectiles;
+
+        /// <summary>
+        /// Render primitives that use pixelation here.
+        /// </summary>
+        void RenderPixelatedPrimitives(SpriteBatch spriteBatch);
+    }
+}
---
+++ Core/Graphics/Primitives/PrimitiveSettings.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using static Luminance.Core.Graphics.PrimitiveSettings;
+using Terraria.Graphics.Shaders;
+
+namespace Luminance.Core.Graphics
+{
+    public record PrimitiveSettings(VertexWidthFunction WidthFunction, VertexColorFunction ColorFunction, VertexOffsetFunction OffsetFunction = null, bool Smoothen = true, bool Pixelate = false, MiscShaderData Shader = null, (Vector2 Left, Vector2 Right)? InitialVertexPositionsOverride = null)
+    {
+        /// <summary>
+        /// A delegate to dynamically determine the width of the trail at each position.
+        /// </summary>
+        /// <param name="trailLengthInterpolant">The current position along the trail as a 0-1 interlopant value.</param>
+        /// <returns>The width for the current point.</returns>
+        public delegate float VertexWidthFunction(float trailLengthInterpolant);
+
+        /// <summary>
+        /// A delegate to dynamically determine the color of the trail at each position.
+        /// </summary>
+        /// <param name="trailLengthInterpolant">The current position along the trail as a 0-1 interlopant value.</param>
+        /// <returns>The color for the current point.</returns>
+        public delegate Color VertexColorFunction(float trailLengthInterpolant);
+
+        /// <summary>
+        /// A delegate to dynamically determine the offset of the trail at each position.
+        /// </summary>
+        /// <param name="trailLengthInterpolant">The current position along the trail as a 0-1 interlopant value.</param>
+        /// <returns>The offset for the current point.</returns>
+        public delegate Vector2 VertexOffsetFunction(float trailLengthInterpolant);
+    }
+}
