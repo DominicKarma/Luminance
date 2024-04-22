32d01ce6faf830c22ba271565eb71881dae8d333
Toasty <louscliff@gmail.com>
2024-04-22 19:01:54 +0100

    - Added verlet stuff.

---
+++ Common/VerletIntergration/VerletSegment.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Luminance.Common.VerletIntergration
+{
+    /// <summary>
+    /// Represents a verlet segment, for use in verlet simulatons.
+    /// </summary>
+    /// <param name="position">The position of this segment.</param>
+    /// <param name="velocity">The velocity of this segment.</param>
+    /// <param name="locked">Whether the segment is locked in place. If true, its position will not update via simulations.</param>
+    public class VerletSegment(Vector2 position, Vector2 velocity, bool locked = false)
+    {
+        public Vector2 Position = position;
+
+        public Vector2 OldPosition = position;
+
+        public Vector2 Velocity = velocity;
+
+        public bool Locked = locked;
+    }
+}
---
+++ Common/VerletIntergration/VerletSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace Luminance.Common.VerletIntergration
+{
+    /// <summary>
+    /// Configurable settings for verlet simulations.
+    /// </summary>
+    /// <param name="TileCollision">Whether the segments should collide with tiles.</param>
+    /// <param name="SlowInWater">Whether the segments should move slower through water.<br/><b>Does nothing if <paramref name="Gravity"/> is 0.</b></param>
+    /// <param name="Gravity">The gravity strength to apply to the segments.</param>
+    /// <param name="MaxFallSpeed">The maximum speed the segments should fall due to gravity.</param>
+    /// <param name="ConserveEnergy">Whether an additional calcuation should be performed to conserve the energy of the segments.<br/>This results in them bouncing around a lot more before settling down.</param>
+    public record VerletSettings(bool TileCollision = false, bool SlowInWater = true, float Gravity = 0.3f, float MaxFallSpeed = 19f, bool ConserveEnergy = false);
+}
