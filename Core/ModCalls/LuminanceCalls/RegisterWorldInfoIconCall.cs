43ee42685e43af6e199f887e376ecd307ca4dd78
tomat <xxlennygamerxx@gmail.com>
2024-04-29 18:57:47 -0700

    Add Mod::Call to register world info icons.

---
+++ Core/ModCalls/LuminanceCalls/RegisterWorldInfoIconCall.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Luminance.Core.MenuInfoUI;
+using Terraria.IO;
+
+namespace Luminance.Core.ModCalls.LuminanceCalls
+{
+    internal sealed class RegisterWorldInfoIconCall : ModCall
+    {
+        private sealed class ModCallInfoUIManager : InfoUIManager
+        {
+            private static readonly List<WorldInfoIcon> worldInfoIcons = [];
+
+            public override IEnumerable<WorldInfoIcon> GetWorldInfoIcons() => worldInfoIcons;
+
+            public static void AddWorldInfoIcon(WorldInfoIcon icon) => worldInfoIcons.Add(icon);
+        }
+        
+        public override IEnumerable<string> GetCallCommands()
+        {
+            yield return "RegisterWorldInfoIcon";
+        }
+
+        public override IEnumerable<Type> GetInputTypes()
+        {
+            yield return typeof(string);
+            yield return typeof(string);
+            yield return typeof(Func<WorldFileData, bool>);
+            yield return typeof(byte);
+        }
+        
+        protected override object SafeProcess(params object[] argsWithoutCommand)
+        {
+            string texturePath = (string)argsWithoutCommand[0];
+            string hoverTextKey = (string)argsWithoutCommand[1];
+            Func<WorldFileData, bool> shouldAppear = (Func<WorldFileData, bool>)argsWithoutCommand[2];
+            byte priority = (byte)argsWithoutCommand[3];
+            
+            ModCallInfoUIManager.AddWorldInfoIcon(new WorldInfoIcon(texturePath, hoverTextKey, shouldAppear, priority));
+        }
+    }
+}
